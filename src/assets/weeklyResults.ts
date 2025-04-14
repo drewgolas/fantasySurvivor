2f77cd83d41ef9a2db11f3a7fbef7d1756ad6643
Drew Schneider <drewgolas@users.noreply.github.com>
2025-04-14 16:00:06 -0400

    Updates

--- src/assets/weeklyResults.ts
+++
@@ -1,9 +0,0 @@
-export const form_results = `Name,Betray,Wins,Voted out,Finds,Uses,Letters
-Will,Mitch,David,Cedrek,Sai,Mitch,No
-Roy,Mitch,David,Sai,Kamilla,Eva,No
-Kevin,Mary,Kyle,Sai,Kyle,Eva,No
-Marco,Kyle,Eva,Sai,Kamilla,Eva,No
-Drew,Chrissy,Eva,Kyle,David,Mitch,No
-Jesse,Mary,Eva,Shauhin,Kamilla,Mitch,Yes
-Chris,Cedrek,Shauhin,Mitch,Shauhin,Mary,Yes
-Kyle,Mary,David,Cedrek,Kyle,Mitch,Yes`
